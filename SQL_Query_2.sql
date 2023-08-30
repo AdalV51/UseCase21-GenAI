bb6de040804df4986d5a9f28ecc208f454d22fd8
AdalV51 <vazquezromanadalberto@gmail.com>
2023-08-30 13:57:49 -0600

    UC#21 SQL_2

---
+++ SQL_Query_2.sql
@@ -0,0 +1,19 @@
+SELECT
+    ProductName,
+    ((SalesRevenue - LAG (SalesRevenue) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC)) / LAG (SalesRevenue) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC)) * 100 AS GrowthRevenueRate,
+    ((SalesQuantity - LAG (SalesQuantity) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC)) / LAG (SalesQuantity) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC)) * 100 AS GrowthQuantityRate,
+    Quarter AS CurrentQuarter,
+    Year AS CurrentYear,
+    LAG (Quarter) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC) AS PreviousQuarter,
+    LAG (Year) OVER (PARTITION BY ProductName ORDER BY Year, Quarter ASC) AS PreviousYear
+FROM (
+    SELECT
+        DATEPART(q, [FS].[Invoice Date Key]) AS [Quarter],
+        DATEPART(year, [FS].[Invoice Date Key]) AS [Year],
+        [DSI].[Stock Item] AS ProductName,
+        CAST(SUM([FS].[Quantity]) AS DECIMAL) AS SalesQuantity,
+        SUM([FS].[Quantity]) * SUM([FS].[Unit Price]) AS SalesRevenue
+    FROM [Dimension].[Stock Item] DSI
+    JOIN [Fact].[Sale] FS ON [DSI].[Stock Item Key] = [FS].[Stock Item Key]
+    GROUP BY DATEPART(year, [FS].[Invoice Date Key]),DATEPART(q, [FS].[Invoice Date Key]), [DSI].[Stock Item]
+) cte
